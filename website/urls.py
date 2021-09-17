8afb628e943cb73197a7680ee05f1523ae41c6b7
Your Name <you@example.com>
2021-09-17 08:50:29 +0000

    Add3

--- website/urls.py
+++ website/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, Article_1
 
 urlpatterns = [
     path('', IndexView.as_view()),
+    path('article_1/', Article_1.as_view()),
     ]
--- website/views.py
+++ website/views.py
@@ -2,3 +2,6 @@ from django.views.generic import TemplateView
 
 class IndexView(TemplateView):
     template_name = "index.html"
+    
+class Article_1(TemplateView):
+    template_name = "article_1.html"
